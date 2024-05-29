5fa8937fdb93ae99e09bad539aceb65d50a39cb6
Yordan Yordanov <yoyordanov94@gmail.com>
2024-05-29 14:11:37 +0300

    deleting more stuff relate to prev commit

--- resources/views/categories/index.blade.php
+++ resources/views/categories/index.blade.php
@@ -1,15 +1,13 @@
 <x-app-layout>
     <x-tabs.body class="flex flex-col gap-4">
         <x-tabs.button-group>
-            <x-tabs.button hx-get="{{ route('categories.loadTab', 'expense') }}" hx-target="#tab-content"
-                           hx-swap="innerHTML">
+            <x-tabs.button>
                 Expense
                 <x-icon class="text-red-500 mt-1">
                     arrow_drop_down
                 </x-icon>
             </x-tabs.button>
-            <x-tabs.button hx-get="{{ route('categories.loadTab', 'income') }}" hx-target="#tab-content"
-                           hx-swap="innerHTML">
+            <x-tabs.button>
                 Income
                 <x-icon class="text-emerald-500 mt-1">
                     arrow_drop_up
@@ -23,9 +21,7 @@
             </x-buttons.card-button>
         </a>
 
-        <x-tabs.content-group id="tab-content" class="mb-4" hx-get="{{ route('categories.loadTab', 'expense') }}"
-                              hx-target="#tab-content"
-                              hx-swap="innerHTML" hx-trigger="load">
+        <x-tabs.content-group class="mb-4">
 
             <x-tabs.content class="flex flex-col gap-4">
                 <x-forms.search>
