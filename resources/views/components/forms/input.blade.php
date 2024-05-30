6d66aa2bdb50504afcb502df295cd3598deaa47e
Yordan Yordanov <yoyordanov94@gmail.com>
2024-05-30 23:32:57 +0300

    wip

--- resources/views/components/forms/input.blade.php
+++ resources/views/components/forms/input.blade.php
@@ -1,5 +1,5 @@
 @props(['disabled' => false])
 
 <input {{ $disabled ? 'disabled' : '' }} {!! $attributes->merge([
-    'class' => 'rounded-lg shadow-sm mt-2 border-gray-300 dark:border-gray-500 dark:bg-neutral-900/50 dark:text-white focus:border-gray-500 focus:ring-gray-500'
+    'class' => 'rounded-lg mt-2 border-neutral-400/50 dark:border-neutral-600/50 dark:bg-neutral-900/50 dark:text-white focus:border-gray-500 focus:ring-gray-500 focus:border-gray-400 focus:ring-gray-400'
     ]) !!}>
--- resources/views/components/panels/panel.blade.php
+++ resources/views/components/panels/panel.blade.php
@@ -1,7 +1,7 @@
 @props(['modal' => false, 'padding' => '8'])
 
 @php
-    $classes = 'rounded-xl border bg-white/20 border-neutral-300/50 dark:border-neutral-800/50 dark:bg-neutral-900/20 dark:shadow p-'.$padding;
+    $classes = 'rounded-xl border bg-white/20 border-neutral-400/50 dark:border-neutral-600/50 dark:bg-neutral-900/20 dark:shadow p-'.$padding;
 
     if(!$modal) {
         $classes .= ' backdrop-blur-lg';
