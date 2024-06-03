e0eec83974b82738dee8945cd9374e60ac196091
Yordan Yordanov <yoyordanov94@gmail.com>
2024-06-03 13:42:04 +0300

    put default categories on queue

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Events\UserRegistered;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Auth\Events\Registered;
--- app/Listeners/CreateDefaultCategories.php
+++ app/Listeners/CreateDefaultCategories.php
@@ -3,17 +3,10 @@
 namespace App\Listeners;
 
 use Illuminate\Auth\Events\Registered;
+use Illuminate\Contracts\Queue\ShouldQueue;
 
-class CreateDefaultCategories
+class CreateDefaultCategories implements ShouldQueue
 {
-    /**
-     * Create the event listener.
-     */
-    public function __construct()
-    {
-        //
-    }
-
     /**
      * Handle the event.
      */
