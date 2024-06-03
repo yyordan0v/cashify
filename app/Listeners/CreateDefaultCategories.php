d4e9cb5b063bfdee6db09963d3464b77b57833c6
Yordan Yordanov <yoyordanov94@gmail.com>
2024-06-03 13:44:56 +0300

    extract default categories in config file

--- app/Listeners/CreateDefaultCategories.php
+++ app/Listeners/CreateDefaultCategories.php
@@ -4,6 +4,7 @@ namespace App\Listeners;
 
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Support\Facades\Config;
 
 class CreateDefaultCategories implements ShouldQueue
 {
@@ -14,118 +15,7 @@ class CreateDefaultCategories implements ShouldQueue
     {
         $user = $event->user;
 
-        $defaultCategories = [
-            // income
-            [
-                'name' => 'Salary',
-                'type' => 'income',
-                'color' => 'indigo',
-                'icon' => 'coin'
-            ],
-            [
-                'name' => 'Gift',
-                'type' => 'income',
-                'color' => 'blue',
-                'icon' => 'piggy-bank'
-            ],
-            // expense
-            [
-                'name' => 'Groceries',
-                'type' => 'expense',
-                'color' => 'green',
-                'icon' => 'groceries'
-            ],
-            [
-                'name' => 'Junk Food',
-                'type' => 'expense',
-                'color' => 'sky',
-                'icon' => 'fast-food'
-            ],
-            [
-                'name' => 'Eating Out',
-                'type' => 'expense',
-                'color' => 'cyan',
-                'icon' => 'food-tray'
-            ],
-            [
-                'name' => 'Gifts',
-                'type' => 'expense',
-                'color' => 'blue',
-                'icon' => 'gift'
-            ],
-            [
-                'name' => 'Clothes',
-                'type' => 'expense',
-                'color' => 'amber',
-                'icon' => 'clothes-hanger'
-            ],
-            [
-                'name' => 'Shoes',
-                'type' => 'expense',
-                'color' => 'lime',
-                'icon' => 'sneakers'
-            ],
-            [
-                'name' => 'Car',
-                'type' => 'expense',
-                'color' => 'rose',
-                'icon' => 'car(1)'
-            ],
-            [
-                'name' => 'Fuel',
-                'type' => 'expense',
-                'color' => 'sky',
-                'icon' => 'gas-station'
-            ],
-            [
-                'name' => 'Insurance/Taxes',
-                'type' => 'expense',
-                'color' => 'orange',
-                'icon' => 'bill'
-            ],
-            [
-                'name' => 'Entertainment',
-                'type' => 'expense',
-                'color' => 'violet',
-                'icon' => 'popcorn'
-            ],
-            [
-                'name' => 'Rent',
-                'type' => 'expense',
-                'color' => 'gray',
-                'icon' => 'rent'
-            ],
-            [
-                'name' => 'Bills',
-                'type' => 'expense',
-                'color' => 'blue',
-                'icon' => 'paper-bill'
-            ],
-            [
-                'name' => 'Healthcare',
-                'type' => 'expense',
-                'color' => 'emerald',
-                'icon' => 'healthcare-and-medical'
-            ],
-            [
-                'name' => 'Pet',
-                'type' => 'expense',
-                'color' => 'green',
-                'icon' => 'dog'
-            ],
-            [
-                'name' => 'Savings',
-                'type' => 'expense',
-                'color' => 'yellow',
-                'icon' => 'money-bag2'
-            ],
-            [
-                'name' => 'Home Appliances',
-                'type' => 'expense',
-                'color' => 'gray',
-                'icon' => 'cleaning'
-            ],
-        ];
+        $defaultCategories = Config::get('default_categories');
 
         foreach ($defaultCategories as $category) {
             $user->categories()->create($category);
