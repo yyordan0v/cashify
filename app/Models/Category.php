69da949f3578b45dd04eef94e43df660d17c0a8b
Yordan Yordanov <yoyordanov94@gmail.com>
2024-06-03 15:21:12 +0300

    wip

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -11,7 +11,7 @@ class Category extends Model
 {
     use HasFactory, HasColor;
 
-//    protected $shade = 200;
+    protected $shade = 200;
 
     protected $fillable = [
         'name',
--- resources/views/components/forms/radio/color.blade.php
+++ resources/views/components/forms/radio/color.blade.php
@@ -6,9 +6,29 @@
 ])
 
 @php
-    $bgColor = 'bg-'.$color.'-300';
+    $colorClasses = [
+        'gray' => 'bg-gray-200',
+        'orange' => 'bg-orange-200',
+        'amber' => 'bg-amber-200',
+        'yellow' => 'bg-yellow-200',
+        'lime' => 'bg-lime-200',
+        'green' => 'bg-green-200',
+        'emerald' => 'bg-emerald-200',
+        'teal' => 'bg-teal-200',
+        'cyan' => 'bg-cyan-200',
+        'sky' => 'bg-sky-200',
+        'blue' => 'bg-blue-200',
+        'indigo' => 'bg-indigo-200',
+        'violet' => 'bg-violet-200',
+        'purple' => 'bg-purple-200',
+        'fuchsia' => 'bg-fuchsia-200',
+        'pink' => 'bg-pink-200',
+        'rose' => 'bg-rose-200',
+     ];
 
-    $classes = 'snap-center inline-flex items-center justify-center w-full p-6 shadow-sm rounded-full cursor-pointer transition-all duration-200 opacity-90 hover:opacity-100 peer-checked:ring-2 dark:peer-checked:ring-4 peer-checked:ring-black dark:peer-checked:ring-white peer-checked:opacity-100 ' . $bgColor;
+     $bgColor = $colorClasses[$color] ?? 'bg-gray-200';
+
+     $classes = 'snap-center inline-flex items-center justify-center w-full p-6 shadow-sm rounded-full cursor-pointer transition-all duration-200 opacity-90 hover:opacity-100 peer-checked:ring-2 dark:peer-checked:ring-4 peer-checked:ring-black dark:peer-checked:ring-white peer-checked:opacity-100 ' . $bgColor;
 @endphp
 
 <li class="my-1">
