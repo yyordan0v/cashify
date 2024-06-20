1178616bda8ac12c2c9cb1f8e17b572841aa4f99
Yordan Yordanov <yoyordanov94@gmail.com>
2024-06-20 22:48:31 +0300

    fix styles

--- resources/views/components/buttons/card-button.blade.php
+++ resources/views/components/buttons/card-button.blade.php
@@ -1,7 +1,7 @@
 @props(['padding' => '4'])
 
 <x-panels.panel
-    {{ $attributes->merge(['class' => 'flex items-center justify-center h-full group transition-colors duration-150 hover:bg-white/40 hover:dark:bg-neutral-700/20 cursor-pointer']) }} padding="{{ $padding }}">
+    {{ $attributes->merge(['class' => 'flex items-center justify-center h-full group transition-colors duration-150 hover:bg-neutral-200/20 hover:dark:bg-neutral-700/20 cursor-pointer']) }} padding="{{ $padding }}">
     <div class="flex flex-col items-center">
         <x-icon class="opacity-20 text-black dark:text-white" style="font-size: 32px">
             format_list_bulleted_add
--- resources/views/components/tabs/button-group.blade.php
+++ resources/views/components/tabs/button-group.blade.php
@@ -1,7 +1,23 @@
 @props(['count' => '2'])
 
+@php
+    $gridColsMapping = [
+        '1' => 'grid-cols-1',
+        '2' => 'grid-cols-2',
+        '3' => 'grid-cols-3',
+        '4' => 'grid-cols-4',
+        '5' => 'grid-cols-5',
+        '6' => 'grid-cols-6',
+        '7' => 'grid-cols-7',
+        '8' => 'grid-cols-8',
+        '9' => 'grid-cols-9',
+    ];
+
+    $gridCols = $gridColsMapping[$count];
+@endphp
+
 <div x-ref="tabButtons"
-    {{ $attributes->merge(['class' => 'relative inline-grid items-center justify-center w-full h-10 grid-cols-'.$count.' p-1 bg-white/80 border border-neutral-200/50 dark:border-neutral-800/50 dark:bg-neutral-900/20 rounded-lg select-none dark:shadow backdrop-blur-lg']) }}
+    {{ $attributes->merge(['class' => 'relative inline-grid items-center justify-center w-full h-10 p-1 bg-white/80 border border-neutral-200/50 dark:border-neutral-800/50 dark:bg-neutral-900/20 rounded-lg select-none dark:shadow backdrop-blur-lg ' . $gridCols]) }}
 >
     {{ $slot }}
 
