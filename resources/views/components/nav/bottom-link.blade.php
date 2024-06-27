6cd89df249c1e264cbab05397ca9df9284d168f4
Yordan Yordanov <yoyordanov94@gmail.com>
2024-06-27 10:03:56 +0300

    change bottom nav

--- resources/views/components/nav/bottom-link.blade.php
+++ resources/views/components/nav/bottom-link.blade.php
@@ -1,21 +1,24 @@
 @props(['active' => false, 'description' => 'button'])
 @php
-    $classes = 'inline-flex flex-col items-center justify-center px-5 hover:bg-black/10 dark:hover:bg-black/30 dark:focus:bg-black/30 focus:bg-black/10 transition-colors duration-200 group';
-    $iconClasses = 'group-hover:text-gray-800 dark:group-hover:text-gray-200 transition-colors duration-200';
+    $classes = 'inline-flex flex-col items-center justify-start mt-1 px-2 transition-colors duration-200 group';
+
+    $iconClasses = 'p-2 group-hover:text-gray-800 dark:group-hover:text-gray-200 transition-colors duration-200 hover:bg-black/10 dark:hover:bg-black/30 dark:focus:bg-black/30 focus:bg-black/10 rounded-lg';
+
+    $descriptionClasses = 'text-black/75 dark:text-white/75 text-2xs';
 
     if ($active) {
-        $classes .= ' dark:bg-black/30 bg-black/10';
-        $iconClasses .= ' text-gray-800 dark:text-gray-200';
+        $descriptionClasses .= ' font-semibold';
+        $iconClasses .= ' text-gray-800 dark:text-gray-200 dark:bg-gray-700 bg-gray-200 ';
     }
     else {
-        $classes .= '';
         $iconClasses .= ' text-gray-500 dark:text-gray-400';
+        $descriptionClasses .= '';
     }
 @endphp
 <a {{ $attributes->merge(['class' => $classes]) }}>
     <x-icon {{ $attributes->merge(['class' => $iconClasses]) }}>
         {{ $slot }}
     </x-icon>
-    <span class="text-black/75 dark:text-white/75 text-2xs mt-1">{{ $description }}</span>
+    <span {{ $attributes->merge(['class' => $descriptionClasses]) }}>{{ $description }}</span>
     <span class="sr-only">{{ $description }}</span>
 </a>
--- resources/views/layouts/bottom-nav.blade.php
+++ resources/views/layouts/bottom-nav.blade.php
@@ -1,5 +1,5 @@
 <div
-    class="hidden md:block fixed z-50 max-w-lg rounded-xl bottom-6 right-6 dark:border-neutral-200/50 border dark:bg-white/80 border-neutral-800/50 bg-black/80 backdrop-blur-sm">
+    class="block fixed z-50 max-w-lg rounded-xl bottom-24 right-4 md:bottom-6 md:right-6 dark:border-neutral-200/50 border dark:bg-white/80 border-neutral-800/50 bg-black/80 backdrop-blur-sm">
     <x-tooltip text="Add Transaction" ref="content">
         <div class="grid h-full max-w-lg grid-cols-1 mx-auto" x-ref="content">
             <a href="{{ route('transactions.create') }}"
@@ -7,7 +7,7 @@
                 <x-icon class="text-white dark:text-black">
                     add
                 </x-icon>
-                <span class="sr-only">Home</span>
+                <span class="sr-only">Add</span>
             </a>
         </div>
     </x-tooltip>
@@ -15,8 +15,8 @@
 
 
 <div
-    class="block md:hidden fixed z-50 w-full h-16 -translate-x-1/2 bottom-0 left-1/2 border-neutral-200/50 border-t bg-white/80 dark:border-neutral-800/50 dark:bg-neutral-900/75 backdrop-blur-lg">
-    <div class="grid h-full max-w-lg grid-cols-5 mx-auto">
+    class="block md:hidden fixed z-50 w-full h-20 -translate-x-1/2 bottom-0 left-1/2 border-neutral-200/50 border-t bg-white/80 dark:border-neutral-800/50 dark:bg-neutral-900/75 backdrop-blur-lg">
+    <div class="grid h-full max-w-lg grid-cols-4 mx-8">
         <x-nav.bottom-link :href="route('dashboard')" :active="request()->routeIs('dashboard')" description="Dashboard">
             grid_view
         </x-nav.bottom-link>
@@ -26,16 +26,6 @@
             payments
         </x-nav.bottom-link>
 
-        <div class="flex items-center justify-center">
-            <a href="{{ route('transactions.create') }}"
-               class="inline-flex items-center justify-center w-14 h-14 rounded-full dark:bg-white/80 bg-black/80 dark:hover:bg-white hover:bg-black dark:focus:bg-white focus:bg-black focus:outline-none group">
-                <x-icon class="text-white dark:text-black">
-                    add
-                </x-icon>
-                <span class="sr-only">New item</span>
-            </a>
-        </div>
-
         <x-nav.bottom-link href="{{ route('accounts.index') }}"
                            :active="request()->routeIs(['accounts.index', 'accounts.edit', 'accounts.create', 'accounts.show'])"
                            description="Accounts">
@@ -43,13 +33,12 @@
         </x-nav.bottom-link>
         <button
             @click="sidebarOpen = !sidebarOpen"
-            class="inline-flex flex-col items-center justify-center px-5 transition-colors duration-200 group">
+            class="inline-flex flex-col items-center justify-start mt-1 px-5 transition-colors duration-200 group">
             <x-icon
-                class="transition-colors duration-200 text-gray-500 dark:text-gray-400">
+                class="p-2 transition-colors duration-200 text-gray-500 dark:text-gray-400">
                 more_horiz
-
             </x-icon>
-            <span class="text-black/75 dark:text-white/75 text-2xs mt-1">More</span>
+            <span class="text-black/75 dark:text-white/75 text-2xs">More</span>
             <span class="sr-only">More</span>
         </button>
     </div>
