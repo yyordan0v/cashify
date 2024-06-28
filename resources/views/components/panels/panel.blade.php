0269b4e3a7558d37f0a4dfd2d50e48c3820b8493
Yordan Yordanov <yoyordanov94@gmail.com>
2024-06-28 14:22:08 +0300

    change panel to have 1rem padding on small screen

--- resources/views/components/panels/panel.blade.php
+++ resources/views/components/panels/panel.blade.php
@@ -1,7 +1,7 @@
 @props(['modal' => false, 'padding' => 'p-8'])
 
 @php
-    $classes = 'rounded-xl border bg-white/20 border-neutral-400/50 dark:border-neutral-600/50 dark:bg-neutral-900/20 dark:shadow '.$padding;
+    $classes = 'rounded-xl border bg-white/20 border-neutral-400/50 dark:border-neutral-600/50 dark:bg-neutral-900/20 dark:shadow p-4 md:'.$padding;
 
     if(!$modal) {
         $classes .= ' backdrop-blur-lg';
