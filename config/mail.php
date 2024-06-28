d698cd13078028e1f4e9a13fd87bb99d6e9755df
Yordan Yordanov <yoyordanov94@gmail.com>
2024-06-28 10:44:48 +0300

    add mailtrap as email service

--- config/mail.php
+++ config/mail.php
@@ -49,6 +49,10 @@ return [
             'local_domain' => env('MAIL_EHLO_DOMAIN'),
         ],
 
+        'mailtrap' => [
+            'transport' => 'mailtrap'
+        ],
+
         'ses' => [
             'transport' => 'ses',
         ],
