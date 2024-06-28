757d668b1a5c5add2ee316e16f6cca3fa1edf448
Yordan Yordanov <yoyordanov94@gmail.com>
2024-06-28 14:15:33 +0300

    adjust app layout padding bottom

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -6,7 +6,7 @@
         <!-- Page Content -->
         <div
             class="antialiased min-h-screen bg-[conic-gradient(at_top,_var(--tw-gradient-stops))] dark:from-black dark:via-black/90 dark:to-black">
-            <main class="p-4 md:ml-68 h-auto pt-20 md:pt-24 pb-12">
+            <main class="p-4 md:ml-68 h-auto pt-20 md:pt-24 pb-20 md:pb-0">
                 {{ $slot }}
             </main>
         </div>
