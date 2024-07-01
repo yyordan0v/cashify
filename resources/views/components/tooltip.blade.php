0ca2a2d6f9fdb28aed45372fcbf5a37ac157b25e
Yordan Yordanov <yoyordanov94@gmail.com>
2024-07-01 16:48:52 +0300

    revert last changes

--- resources/views/components/tooltip.blade.php
+++ resources/views/components/tooltip.blade.php
@@ -7,34 +7,17 @@
 
 <div
     x-data="{
-        tooltipVisible: false,
-        tooltipText: '{{ $text }}',
-        tooltipArrow: '{{ $arrow }}',
-        tooltipPosition: '{{ $position }}',
-        isMobile: window.innerWidth < 768,
-        toggleTooltip(event) {
-            if (this.isMobile) {
-                event.preventDefault();
-                event.stopPropagation();
-                this.tooltipVisible = !this.tooltipVisible;
-            }
-        }
-    }"
-    x-init="
-        $refs.{{ $ref }}.addEventListener('mouseenter', () => { if (!isMobile) tooltipVisible = true; });
-        $refs.{{ $ref }}.addEventListener('mouseleave', () => { if (!isMobile) tooltipVisible = false; });
-        $refs.{{ $ref }}.addEventListener('click', toggleTooltip);
-        window.addEventListener('resize', () => { isMobile = window.innerWidth < 768; });
-    "
+            tooltipVisible: false,
+            tooltipText: '{{ $text }}',
+            tooltipArrow: '{{ $arrow }}',
+            tooltipPosition: '{{ $position }}',
+        }"
+    x-init="$refs.{{ $ref }}.addEventListener('mouseenter', () => { tooltipVisible = true; }); $refs.{{ $ref }}.addEventListener('mouseleave', () => { tooltipVisible = false; });"
     class="relative"
 >
-    <div x-ref="{{ $ref }}" class="cursor-pointer">
-        {{ $slot }}
-    </div>
-
     <div x-ref="tooltip" x-show="tooltipVisible"
          :class="{ 'top-0 left-1/2 -translate-x-1/2 -mt-0.5 -translate-y-full' : tooltipPosition == 'top', 'top-1/2 -translate-y-1/2 -ml-0.5 left-0 -translate-x-full' : tooltipPosition == 'left', 'bottom-0 left-1/2 -translate-x-1/2 -mb-0.5 translate-y-full' : tooltipPosition == 'bottom', 'top-1/2 -translate-y-1/2 -mr-0.5 right-0 translate-x-full' : tooltipPosition == 'right' }"
-         class="absolute w-auto text-sm z-10" x-cloak>
+         class="absolute w-auto text-sm" x-cloak>
         <div x-show="tooltipVisible" x-transition
              class="relative px-2 py-1 text-white dark:text-black bg-black dark:bg-white rounded bg-opacity-80">
             <p x-text="tooltipText" class="flex-shrink-0 block text-xs whitespace-nowrap"></p>
@@ -47,4 +30,6 @@
             </div>
         </div>
     </div>
+
+    {{ $slot }}
 </div>
